d9b92cb80b258a20738034f6d8147391ce659ab5
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-14 11:15:45 +0200

    Register page and styling

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,13 @@
+import styles from './register.module.css';
+
+const RegisterPage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <RegisterForm />
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
