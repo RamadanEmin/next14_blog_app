db80684079a063a16ab178363d4c2bfd50ef7980
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-17 19:52:36 +0200

    Added Admin page

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,26 @@
+const AdminPage = async () => {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.row}>
+                <div className={styles.col}>
+                    <AdminPosts />
+                </div>
+                <div className={styles.col}>
+                    <AdminPostsForm />
+                </div>
+            </div>
+
+            <div className={styles.row}>
+                <div className={styles.col}>
+                    <AdminUsers />
+                </div>
+                <div className={styles.col}>
+                    <AdminUserForm />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminPage;
