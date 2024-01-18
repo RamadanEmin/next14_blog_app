a80f0b2a82a4c35f88aaafda18d37c6345a7197d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-18 19:40:59 +0200

    Added Add and delete post actions

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,9 +1,47 @@
 'use server';
 
-import { User } from './models';
+import { revalidatePath } from 'next/cache';
+import { Post, User } from './models';
 import { connectToDb } from './utils';
 import { signIn, signOut } from './auth';
 import bcrypt from 'bcryptjs';
+
+export const addPost = async (prevState, formData) => {
+    const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+    try {
+        connectToDb();
+        const newPost = new Post({
+            title,
+            desc,
+            slug,
+            userId
+        });
+
+        await newPost.save();
+        console.log('saved to db');
+        revalidatePath('/blog');
+        revalidatePath('/admin');
+    } catch (err) {
+        console.log(err);
+        return { error: 'Something went wrong!' };
+    }
+};
+
+export const deletePost = async (formData) => {
+    const { id } = Object.fromEntries(formData);
+
+    try {
+        connectToDb();
+        await Post.findByIdAndDelete(id);
+        console.log('deleted from db');
+        revalidatePath('/blog');
+        revalidatePath('/admin');
+    } catch (err) {
+        console.log(err);
+        return { error: 'Something went wrong!' };
+    }
+};
 
 export const handleGithubLogin = async () => {
     await signIn('github');
